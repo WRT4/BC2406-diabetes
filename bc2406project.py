cf3d8d2d29c51837fa27e3bed0cfe01db719ec30
WRT4 <165753918+WRT4@users.noreply.github.com>
2024-10-23 16:40:32 +0800

    Update bc2406project.py

--- bc2406project.py
+++ bc2406project.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
