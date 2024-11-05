dde00e9d8ff788140e46fd817cd4bbce86c61cad
Wen Rong <165753918+WRT4@users.noreply.github.com>
2024-11-05 18:04:48 +0800

    Update bc2406project.py

--- bc2406project.py
+++ bc2406project.py
@@ -258,7 +258,7 @@ if st.button("Calculate"):
         st.warning("Please visit the doctor!")
     else:
         st.success("No immediate concerns.")
-    if cart_pred2 or rd_pred2:
+    if cart_pred2[0] or rd_pred2[0]:
         st.warning("Please try to improve your lifestyle!")
 st.markdown(
     """
